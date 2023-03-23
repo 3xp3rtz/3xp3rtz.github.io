482889f298885166b4f17508218bd95b694c539d
3xp3rtz <a343063004@gmail.com>
2023-03-23 08:45:27 -0400

    update

--- math-notes.js
+++ math-notes.js
@@ -3,5 +3,6 @@ function loadPage() {
         page = window.location.toString().split('?')[1].substring(7);
         document.getElementById("lesson-" + page).style.visibility = "visible";
         document.getElementById("lesson-" + page).style.display = "block";
+        document.getElementById("lesson-button-" + page).style.backgroundColor = "rgb(171, 124, 41)";
     } catch (error) {}
 }
