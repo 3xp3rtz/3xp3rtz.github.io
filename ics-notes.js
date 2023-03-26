9b238f3139e1531b390173d189b9ad959d2c3b07
3xp3rtz <a343063004@gmail.com>
2023-03-26 13:51:47 -0400

    changed nav-bar, css change

--- ics-notes.js
+++ ics-notes.js
@@ -1,6 +1,6 @@
 function loadPage() {
     try {
-        page = window.location.toString().split('?')[1].substring(5);
+        page = window.location.toString().split('?')[1].substring(5).split('#')[0];
         document.getElementById("content-" + page).style.visibility = "visible";
         document.getElementById("content-" + page).style.display = "block";
     } catch (error) {}
